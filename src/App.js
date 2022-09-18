f92844495f4eead59b9114d787e4c711f07a856c
Himanshu Negi <himanshu.21negi@gmail.com>
2022-09-18 21:40:53 +0530

    Second phase completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import HeadBanner from './components/head-banner/HeadBanner'
+import MidContent from './components/MiddleContent/MidContent';
 
 function App() {
   return (
     <div className="App">
       <HeadBanner/>
+      <div className='app_style'>
+      <MidContent/>
+      </div>
     </div>
   );
 }
