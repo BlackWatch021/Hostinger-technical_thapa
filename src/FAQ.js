04f66a9333c7dee4a2e88a6417a89485adf45236
Himanshu Negi <himanshu.21negi@gmail.com>
2022-09-21 19:44:55 +0530

    FAQ part added

--- src/FAQ.js
+++ src/FAQ.js
@@ -90,3 +90,9 @@ export const faqAPI = [
     If you're having trouble, our Customer Success team will gladly assist you.`,
   },
 ];
+
+export const svg = (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="white">
+    <path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" />
+  </svg>
+);
--- src/components/FAQ/Faq.js
+++ src/components/FAQ/Faq.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Faq.css";
-import { faqAPI } from "../../FAQ";
-
-console.log(faqAPI);
+import { faqAPI, svg } from "../../FAQ";
 
 const Faq = () => {
+  let [rotateSVG, setRotateSVG] = useState();
+  let [hideText, setHideText] = useState({});
+
+  let [clicked, setClicked] = useState(false);
+
+  const rotate = () => {
+    if (clicked) {
+      setClicked(false);
+      setRotateSVG({});
+      setHideText({});
+    } else {
+      setClicked(true);
+      setRotateSVG({ transform: "rotate(180deg)" });
+      setHideText({ display: "none" });
+    }
+  };
+
   return (
     <>
+      <div className="faq_container">
+        <h1>Website Hosting India FAQs</h1>
+        <p>
+          Find answers to frequently asked questions about website hosting and
+          our services.
+        </p>
+        {faqAPI.map((el, id) => {
+          return (
+            <div key={id} className="faq_wrapper">
+              <div className="faq_wrapper_head">
+                <h2>{el.heading}</h2>
+                <div onClick={rotate} style={rotateSVG}>
+                  {svg}
+                </div>
+              </div>
+              <p style={hideText}>{el.text}</p>
+            </div>
+          );
+        })}
+      </div>
       {/* {faqAPI.map((el) => {
         return (
           <div>
