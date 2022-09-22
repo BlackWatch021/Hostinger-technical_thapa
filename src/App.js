1875da9af56678399e328e64b76d61621450faaa
Himanshu Negi <himanshu.21negi@gmail.com>
2022-09-22 18:53:38 +0530

    most of the work is doen

--- src/App.js
+++ src/App.js
@@ -6,15 +6,19 @@ import AdditionalDetails from "./components/AdditionalDetails/AdditionalDetails"
 import Faq from "./components/FAQ/Faq";
 import Return from "./components/ReturnPolicy/Return";
 import Footer from "./components/Footer/Footer";
+import Rating from "./components/Rating/Rating";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <HeadBanner />
       <div className="app_style">
         <MidContent />
         <Plans />
         <Return />
+        <Rating />
         <AdditionalDetails />
         <Faq />
       </div>
---
+++ src/Rating.js
@@ -0,0 +1,91 @@
+export const clientRatings = [
+  {
+    starts: 5,
+    client: "Satish Chandra Gupta",
+    date: "September 22, 2022",
+    title: "Hostinger is one of the best hosting",
+    text: `Hostinger is one of the best hosting providers. I've been working with Hostinger for the last 2 years and 10 months, they have helped and supported me with every issue.`,
+  },
+
+  {
+    starts: 5,
+    client: "Lalit",
+    date: "September 22, 2022",
+    title: "All good!...",
+    text: `All good!...`,
+  },
+
+  {
+    starts: 5,
+    client: "Nafis Siam Siam",
+    date: "September 23, 2021",
+    title: "One of the best Hosting Service Provider",
+    text: `No Doubt "Hostinger" one of the best hosting provider in the world.I am very satisfied with their hosting service.but their live chat support is very poor.It takes a long time to get their live chat support.`,
+  },
+  {
+    starts: 5,
+    client: "Thodreque",
+    date: "September 20, 2022",
+    title: "Great service, customer care at...",
+    text: `Great service, customer care at competitive rates`,
+  },
+  {
+    starts: 4,
+    client: "Furokai",
+    date: "September 21, 2022",
+    title: "wanted to become informed",
+    text: `wanted to become informed about new offers on Hostinger. All my questions have been answered.`,
+  },
+  {
+    starts: 5,
+    client: "Gourav Saha",
+    date: "September 21, 2022",
+    title:
+      "wanted to become informed about new offers on Hostinger. All my questions have been answered.",
+    text: `Very nice service & speed of the server is very good.`,
+  },
+  {
+    starts: 4,
+    client: "Luis Alejandro Araúz Justinian",
+    date: "March 01, 2022",
+    title: "Almost perfect, has some quirks",
+    text: `It would get a perfect review from me, but live support chat has been unavailable for a while, they only offer an FAQ and email support. VPS plans don't offer alternatives to CentOS 8 yet.`,
+  },
+  {
+    starts: 5,
+    client: "Ravi RV",
+    date: "September 21, 2022",
+    title: "Nice place to host your website with…",
+    text: `Nice place to host your website with good performance`,
+  },
+  {
+    starts: 5,
+    client: "manuelabis",
+    date: "September 21, 2022",
+    title: "What I like the most is Hostinger is…",
+    text: `What I like the most is Hostinger is able to give me the full capability of a website, whether business or personal, whether as a simple blogger or creating a magnificent landing page.`,
+  },
+  {
+    starts: 5,
+    client: "Tony",
+    date: "September 20, 2022",
+    title: "Great hosting company",
+    text: `Great company if you want to start a business.
+    The menu and options available are fantastic for the price I pay.`,
+  },
+  {
+    starts: 5,
+    client: "Arun",
+    date: "September 20, 2022",
+    title: "I recommend this service for many…",
+    text: `I recommend this service for many people only because of nice solutions and services provided`,
+  },
+
+  {
+    starts: 5,
+    client: "Don Findlay",
+    date: "September 16, 2022",
+    title: "Hostinger rocks",
+    text: `Hostinger rocks. Fast, reliable servers, all the tools you need for registering domains and building web sites, great customer service.`,
+  },
+];
